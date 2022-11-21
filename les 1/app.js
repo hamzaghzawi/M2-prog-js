4ee92b6d54fe8e3e1aecaf8e75dbbdf5d8ace178
Hamza <34681@ma-web.nl>
2022-11-21 10:50:42 +0100

    js

---
+++ les 1/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        //code gaat hier
+let appNaam = "Waarde"; 
+const versienummer = 0.9 ;
+let versiedatum = "21-11-2022"
+let autheur = "Bas"
+let copyright = "h"
+let distributeur = "waarde"
+const darkmode = "bool"
+console.log(appNaam);
+console.log(versienummer);
+console.log(autheur);
+console.log(copyright);
+console.log(distributeur);
+console.log(darkmode);
+    }
+}
+
+let app = new App();
+app.runApplication();
